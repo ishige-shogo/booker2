97085083cad41551d2a9add055f34c369270705d
ishige-shogo <shigeshigeishige@gmail.com>
2020-11-09 01:19:32 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
 
   devise_for :users
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
