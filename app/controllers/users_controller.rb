a2e018f183671306d365fee70c4be53faa2ef3e5
ishige-shogo <shigeshigeishige@gmail.com>
2020-11-10 05:14:44 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6949da6a97845fe180073b06be4de9bef0ad29793e7f7be4301e6d449fa1a45375606711d70838edbcfc008e498d433f7caa44d8d7d2a6e10ac56e9ad5305894'
